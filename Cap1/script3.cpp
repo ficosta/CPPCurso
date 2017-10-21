e60fe62bf9ceeeae4ddd126c3b2d295f1297a631
ficosta <felipeiasi@gmail.com>
2017-10-21 14:35:44 +0000

    Arrumando arquivos

---
+++ Cap1/script3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int idade;
+	cout << "Digite sua idade: ";
+	cin >> idade;
+
+	cout << "Sua idade eh " << idade << '\n';
+
+	return 0;
+}
